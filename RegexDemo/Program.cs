a10f8d4043ecd7d91b85c3bef546ab2f99369b35
sipra <sipramishra085@gmail.com>
2023-07-10 08:44:25 +0530

    [Sipra]Add:Using Regex Expreesion

---
+++ RegexDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+          
+            RegexExample regexExample = new RegexExample();
+            regexExample.MyRegex();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexDemo/RegexExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public  class RegexExample
+    {
+        static string pattern = "^cen$";
+        //^it will indicate string start
+        //$it will indicate the end of the string
+
+        public void MyRegex()
+        {
+            //we have created object of class
+            //we have given a pattern in given class constuctor
+            Regex regex = new Regex(pattern);
+
+            //ismatch return true if its matches the string otherwise it return false.
+            if (regex.IsMatch(""))
+            {
+                Console.WriteLine("string is match the pattern");
+            }
+            else
+            {
+                Console.WriteLine("string is not  match the pattern");
+
+            }
+        }
+    }
+}
